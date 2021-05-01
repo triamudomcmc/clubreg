40b31eff0f2be025f0e26fb99bd07c37baea5190
Peter <peterphongpak@gmail.com>
2021-05-01 21:10:44 +0700

    Fix build error

--- src/pages/clubs/index.tsx
+++ src/pages/clubs/index.tsx
@@ -2,8 +2,12 @@ import PageContainer from "@components/common/PageContainer";
 import ClubSplash from "@vectors/decorations/ClubSplash";
 import {FilterSearch} from "@components/common/Inputs/Search";
 import {ClubCard} from "@components/clubs/ClubCard";
+import {useState} from "react";
 
 const Clubs = () => {
+  const [sortMode, setSortMode] = useState("ascending")
+  const [searchContext, setSearchContext] = useState("")
+
   return (
     <PageContainer>
       <div className="flex flex-col items-center w-full py-12 md:py-20">
@@ -14,7 +18,7 @@ const Clubs = () => {
           </div>
         </div>
         <div className="mt-8 md:mt-12 pb-4 border-b mx-8 md:mx-0 md:border-none md:px-8 md:w-full max-w-xl">
-          <FilterSearch/>
+          <FilterSearch setSearchContext={setSearchContext} setSortMode={setSortMode} sortMode={sortMode}/>
         </div>
         <div className="flex flex-wrap w-full justify-center max-w-5xl mt-5 md:mt-14">
           <ClubCard audition={true}/>
