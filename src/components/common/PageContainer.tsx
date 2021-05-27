a9565acaad1fa02e517ea55768e143dab169c43c
Peter <peterphongpak@gmail.com>
2021-05-28 00:56:08 +0700

    Add page transition

--- src/components/common/PageContainer.tsx
+++ src/components/common/PageContainer.tsx
@@ -1,11 +1,34 @@
 import Navigation from "@components/common/Navigation";
 import Footer from "@components/common/Footer";
+import {AnimatePresence, motion} from "framer-motion";
+import {useRouter} from "next/router";
 
 const PageContainer = ({children, footer = true}) => {
+
+  const router = useRouter()
+
+  const variants = {
+    initial: {y: -20, opacity: 0},
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+        ease: [0.6, -0.05, 0.01, 0.99],
+      },
+    },
+  };
+
   return (
     <div className="font-display">
       <Navigation/>
-      {children}
+      <motion.div initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={variants}
+                  key={router.pathname}>
+        {children}
+      </motion.div>
       {footer && <Footer/>}
     </div>
   )
