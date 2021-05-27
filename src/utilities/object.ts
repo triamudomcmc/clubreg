4aab0eb6c97cfae315231c0408f7e89af3534043
Peter <peterphongpak@gmail.com>
2021-05-28 02:02:07 +0700

    fix error

--- src/utilities/object.ts
+++ src/utilities/object.ts
@@ -2,8 +2,7 @@ import {clubMap} from "../config/clubMap";
 import LooseTypeObject from "@interfaces/LooseTypeObject";
 
 export const isEmpty = (obj: LooseTypeObject<any> | undefined | null) => {
-  if (!obj) return false
-  return Object.keys(obj).length == 0
+  return obj && Object.keys(obj).length == 0
 }
 
 export const sliceObj = (obj, partitions) => {
