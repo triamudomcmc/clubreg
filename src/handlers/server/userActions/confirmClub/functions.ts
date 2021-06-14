f6a5801c126a6efcb7fdc29e6deabc879eae9194
Peter <peterphongpak@gmail.com>
2021-06-15 01:59:08 +0700

    Prevent duplicated request

--- src/handlers/server/userActions/confirmClub/functions.ts
+++ src/handlers/server/userActions/confirmClub/functions.ts
@@ -5,6 +5,7 @@ import {firestore} from "firebase-admin/lib/firestore";
 export const checkInputs = async (dataDoc, userData, req) => {
   if (!(req.body.clubID in dataDoc.get("audition")) || dataDoc.get("club") !== "") return {status: false, report: "not_audition"}
   if (dataDoc.get("audition")[req.body.clubID] !== "passed") return {status: false, report: "not_pass"}
+  if (dataDoc.get("audition")[req.body.clubID] === "confirmed") return {status: false, report: "confirmed"}
   if (userData.get("phone") !== req.body.phone) return {status: false, report: "invalid_phone"}
   if (!(await bcrypt.compare(req.body.password, userData.get("password")))) return {
     status: false, report: "invalid_password"
