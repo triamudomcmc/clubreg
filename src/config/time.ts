a4c240ade91be6dbdfd523400efc5e231db69ab6
Peter <peterphongpak@gmail.com>
2021-06-14 20:18:36 +0700

    Fix timer

--- src/config/time.ts
+++ src/config/time.ts
@@ -1,4 +1,4 @@
 export const openTime = 1623043800000
-export const editDataTime = 1623711600000
+export const editDataTime = 1623690000000
 export const announceTime = 1623717000000
 export const endRegClubTime = 1623690000000
