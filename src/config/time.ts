e386e813ccb34ed57a0e8b17b04f8b4a1ec35b68
Peter <peterphongpak@gmail.com>
2021-06-16 00:22:45 +0700

    Enable reserve swap

--- src/config/time.ts
+++ src/config/time.ts
@@ -2,6 +2,6 @@ export const openTime = 1623043800000
 export const editDataTime = 1623690000000
 export const announceTime = 1623717000000
 export const endRegClubTime = 1623690000000
-export const positionUpdateTime = editDataTime
 export const breakUpperBound = 1623778200000
 export const breakLowerBound = 1623776400000
+export const positionUpdateTime = 1623798000000
