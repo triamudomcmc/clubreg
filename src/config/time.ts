0f71c05168c4651275358aa4dacd9718e6d56a9f
Peter <peterphongpak@gmail.com>
2021-06-17 00:25:11 +0700

    Add time break

--- src/config/time.ts
+++ src/config/time.ts
@@ -2,6 +2,6 @@ export const openTime = 1623043800000
 export const editDataTime = 1623690000000
 export const announceTime = 1623717000000
 export const endRegClubTime = 1623690000000
-export const breakUpperBound = 162386460000
+export const breakUpperBound = 1623864600000
 export const breakLowerBound = 1623862800000
 export const positionUpdateTime = 1623884400000
