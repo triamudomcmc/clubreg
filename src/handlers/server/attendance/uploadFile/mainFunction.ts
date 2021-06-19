f8419ededd59f2bbd7a9b6a7cee4a9ce3fbc78d0
Peter <peterphongpak@gmail.com>
2021-06-19 23:38:20 +0700

    Fix env

--- src/handlers/server/attendance/uploadFile/mainFunction.ts
+++ src/handlers/server/attendance/uploadFile/mainFunction.ts
@@ -6,8 +6,8 @@ export const performUpload = async (req, ID) => {
   const storage = new Storage({
     projectId: process.env.PROJECT_ID,
     credentials: {
-      client_email: process.env.CLOUD_CLIENT_EMAIL,
-      private_key: process.env.CLOUD_PRIVATE_KEY,
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY,
     },
   });
 
