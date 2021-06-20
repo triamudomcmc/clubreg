0d229d83e0a14d3e57dfd7004303f7e7761aa6a5
Peter <peterphongpak@gmail.com>
2021-06-21 02:50:05 +0700

    Fix timezone

--- src/config/time.ts
+++ src/config/time.ts
@@ -17,7 +17,9 @@ function calcTime(offset) {
 
 export const getPrevMonday = () => {
   let prevMonday = calcTime(7)
+  const offset = prevMonday.getTimezoneOffset()
+
   prevMonday.setDate(prevMonday.getDate() - (prevMonday.getDay() + 6) % 7);
   const timePart = ((prevMonday.getHours()) * 60 * 60 * 1000) + ((prevMonday.getMinutes()) * 60 * 1000) + ((prevMonday.getSeconds()) * 1000) + prevMonday.getMilliseconds()
-  return prevMonday.getTime() - timePart
+  return prevMonday.getTime() - timePart + (7 + (offset * 60 * 1000))
 }
