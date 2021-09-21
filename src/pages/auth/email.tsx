651a05bdca1e2e11c7056e98a112a73983e6d0ec
pryter <peterphongpak@gmail.com>
2021-09-21 16:53:59 +0700

    Add secret

---
+++ src/pages/auth/email.tsx
@@ -0,0 +1,17 @@
+import {GetStaticProps} from "next";
+import Router from "next/router";
+import {useEffect} from "react";
+import {Loader} from "@components/common/Loader";
+
+const Email = () => {
+
+  useEffect(() => {
+    Router.push("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  },[])
+
+  return (
+    <Loader display={true}/>
+  )
+}
+
+export default Email
