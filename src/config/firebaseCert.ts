064c04a296ecb6c533e90278ef2d5e0c09f1d709
betich <betich365@gmail.com>
2022-04-06 21:22:51 +0700

    Add sandbox env handling

--- src/config/firebaseCert.ts
+++ src/config/firebaseCert.ts
@@ -3,10 +3,18 @@ import * as dotenv from "dotenv"
 const firebaseCert = () => {
   dotenv.config()
 
-  return {
-    projectId: process.env.FCERT_PROJECT_ID,
-    private_key: process.env.FCERT_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.FCERT_CLIENT_EMAIL,
+  if (process.env.MODE === "SANDBOX") {
+    return {
+      projectId: process.env.FCERT_PROJECT_ID_SB,
+      private_key: process.env.FCERT_PRIVATE_KEY_SB.replace(/\\n/g, "\n"),
+      client_email: process.env.FCERT_CLIENT_EMAIL_SB,
+    }
+  } else {
+    return {
+      projectId: process.env.FCERT_PROJECT_ID,
+      private_key: process.env.FCERT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.FCERT_CLIENT_EMAIL,
+    }
   }
 }
 
