1f6cf37efa20e510f1f8f2b9cd5608cccc9e8f12
betich <betich365@gmail.com>
2022-04-08 01:06:36 +0700

    Change obsolete url

--- src/pages/api/renderCard.ts
+++ src/pages/api/renderCard.ts
@@ -8,10 +8,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   // console.log(id)
 
-  const file = await screenshot(`http://preview.tucmc.dev/renderer/card?id=${id}`)
+  const file = await screenshot(`${getProtocol(req)}://${req.headers.host}/renderer/card?id=${id}`)
 
   res.setHeader("Content-Type", `image/png`)
   res.setHeader("Cache-Control", `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`)
   res.statusCode = 200
   res.end(file)
 }
+
+const getProtocol = (req: NextApiRequest) => (req.headers.host?.includes("localhost") ? "http" : "https")
