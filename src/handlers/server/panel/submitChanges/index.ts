e0b6c39c6066b35dc98a7875724ae61b2a39d4e2
betich <betich365@gmail.com>
2022-04-17 20:39:50 +0700

    Add a clubDisplayRequests action in the backend

--- src/handlers/server/panel/submitChanges/index.ts
+++ src/handlers/server/panel/submitChanges/index.ts
@@ -29,7 +29,7 @@ const upload = async (image: string, storage, tempFileName) => {
   return res
 }
 
-const performUpload = async (req) => {
+const performUpload = async (req, ID) => {
   const storage = new Storage({
     projectId: process.env.PROJECT_ID_R,
     credentials: {
@@ -52,42 +52,58 @@ const performUpload = async (req) => {
   }
 
   let reviews = []
-  
+
   for (let rev of req.body.reviews) {
-      if (rev.profile.includes("data:image")) {
-        const tempFileName = `userUpload-${new Date().getTime()}-${Math.floor(Math.random() * 400)}`
-        const res = await upload(rev.profile, storage, tempFileName)
-        reviews.push({...rev, profile: `https://storage.googleapis.com/clwimages/${tempFileName}`})
-      }else{
-          reviews.push(rev)
-      }
+    if (rev.profile.includes("data:image")) {
+      const tempFileName = `userUpload-${new Date().getTime()}-${Math.floor(Math.random() * 400)}`
+      const res = await upload(rev.profile, storage, tempFileName)
+      reviews.push({ ...rev, profile: `https://storage.googleapis.com/clwimages/${tempFileName}` })
+    } else {
+      reviews.push(rev)
+    }
   }
 
   const clubDataDoc = initialisedDB.collection("clubs").doc("mainData")
   const out = await clubDataDoc.update({ [`${req.body.panelID}.status`]: "pending" })
 
   const dat = await initialisedDB.collection("clubDisplay").doc(req.body.panelID).get()
-  await initialisedDB.collection("clubDisplay").doc(req.body.panelID).set(
-    {
-    ...(req.body.contact),
+  await initialisedDB
+    .collection("clubDisplay")
+    .doc(req.body.panelID)
+    .set(
+      {
+        ...req.body.contact,
+        reviews: reviews,
+        description: req.body.main,
+        images: { ...(dat.get("images") || {}), ...nim },
+      },
+      { merge: true }
+    )
+
+  const clubDisplayRequestDoc = initialisedDB.collection("clubDisplayRequests").doc()
+  const cDisplayRequestOut = clubDisplayRequestDoc.create({
+    clubID: req.body.panelID,
+    timeStamp: +new Date(),
+    userID: ID.userID,
+    data: {
+      ...req.body.contact,
       reviews: reviews,
       description: req.body.main,
-      images: {...(dat.get("images") || {}), ...nim},
+      images: { ...(dat.get("images") || {}), ...nim },
     },
-    { merge: true }
-  )
+  })
 
   return null
 }
 
-const main = async (req) => {
-  performUpload(req)
+const main = async (req, ID) => {
+  performUpload(req, ID)
 
   return { status: true, report: "success", data: { url: "" } }
 }
 
 export const submitChanges = async (req, res) => {
   return await executeWithPermission(req, res, async (req, res, ID) => {
-    return await main(req)
+    return await main(req, ID)
   })
 }
