cf2dbb393a8c55cfa0caca8b2fbeb5ca6b8f386d
pryter <peterphongpak@gmail.com>
2022-04-17 21:58:27 +0700

    Fix ssr errors

---
+++ src/utilities/getImages.ts
@@ -0,0 +1,317 @@
+const mapped = {
+    'ก30901': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30901-1': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30901-2': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30901-3': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30902': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30903-1': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg'
+    ],
+    'ก30903-2': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30903-3': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30903-4': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30903-5': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30904': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30905': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30905-1': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30905-10': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30905-11': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30905-13': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30905-14': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30905-2': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30905-3': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30905-4': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30905-6': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30905-7': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30905-8': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30905-9': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30909': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30909-1': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30910': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30911': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30912': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30913': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30914': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30915': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30916': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30918': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg'
+    ],
+    'ก30920': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30920-8': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30921': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30922': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30923': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30924': [ 'picture-1.jpg', 'profile-1.jpg' ],
+    'ก30925': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30927': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30928': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30932': [
+      'picture-1.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30934': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30937': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30941': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg'
+    ],
+    'ก30942': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30943': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30945': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30946': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30947': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30948': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30950': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30951': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg'
+    ],
+    'ก30952-1': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30952-2': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30952-3': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg',
+      'profile-3.jpg'
+    ],
+    'ก30952-4': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg'
+    ],
+    'ก30952-5': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg'
+    ],
+    'ก30952-6': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg'
+    ],
+    'ก30952-7': [ 'picture-1.jpg', 'picture-2.jpg', 'profile-1.jpg' ],
+    'ก30953': [
+      'picture-1.jpg',
+      'picture-2.jpg',
+      'picture-3.jpg',
+      'profile-1.jpg',
+      'profile-2.jpg'
+    ]
+  }
+
+export const getImages = (id: string) => {
+    if (id in mapped) {
+        return mapped[id]
+    }else{
+        return []
+    }
+}
