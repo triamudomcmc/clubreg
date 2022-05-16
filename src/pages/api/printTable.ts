96e721192849b4e1ff0807e8a855963857435897
pryter <peterphongpak@gmail.com>
2022-05-17 02:39:37 +0700

    Fix api size

--- src/pages/api/printTable.ts
+++ src/pages/api/printTable.ts
@@ -1,31 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import chrome from "chrome-aws-lambda"
-import puppeteer from "puppeteer-core"
 import initialisedDB from "@server/firebase-admin"
-
-async function savePDF(url: string) {
-  const options = process.env.AWS_REGION
-    ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      }
-    : {
-        args: [],
-        executablePath:
-          process.platform === "win32"
-            ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-            : process.platform === "linux"
-            ? "/usr/bin/google-chrome"
-            : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-      }
-  const browser = await puppeteer.launch(options)
-  const page = await browser.newPage()
-
-  await page.goto(url, { waitUntil: "networkidle2" })
-  
-  return await page.pdf({format: "a4"})
-}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const {
--- src/utilities/files.ts
+++ src/utilities/files.ts
@@ -1,6 +1,33 @@
+import chrome from "chrome-aws-lambda"
+import puppeteer from "puppeteer-core"
+
 export const toBase64 = file => new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
 });
+
+export async function savePDF(url: string) {
+    const options = process.env.AWS_REGION
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: [],
+          executablePath:
+            process.platform === "win32"
+              ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+              : process.platform === "linux"
+              ? "/usr/bin/google-chrome"
+              : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        }
+    const browser = await puppeteer.launch(options)
+    const page = await browser.newPage()
+  
+    await page.goto(url, { waitUntil: "networkidle2" })
+    
+    return await page.pdf({format: "a4"})
+  }
