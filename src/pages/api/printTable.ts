58e4ffdc6a3fb0ab3e4b200aff474a400b75c9da
pryter <peterphongpak@gmail.com>
2022-05-17 02:40:00 +0700

    Fix error

--- src/pages/api/printTable.ts
+++ src/pages/api/printTable.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import initialisedDB from "@server/firebase-admin"
+import { savePDF } from "@utilities/files"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const {
