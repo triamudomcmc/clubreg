dcc1706376a0422a5ae70d87228ba52fa7d041d1
pryter <peterphongpak@gmail.com>
2023-05-13 19:04:00 +0700

    feat: fix pwa sw.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require("next-pwa")
 
 module.exports = withPWA({
   pwa: {
-    swSrc: 'service-worker.js',
+    sw: 'service-worker.js',
     customWorkerDir: 'worker',
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
