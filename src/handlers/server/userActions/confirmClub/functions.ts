1ecd345e3649b921564980a9f6f053f93a191c02
pryter <peterphongpak@gmail.com>
2023-05-30 09:41:32 +0700

    fix: confirm bug

--- src/handlers/server/userActions/confirmClub/functions.ts
+++ src/handlers/server/userActions/confirmClub/functions.ts
@@ -50,9 +50,9 @@ export const createNewAuditionData = async (dataDoc, req, clubRef, t) => {
         newAuditionData[key] = "failed"
       } else {
         if (updatedItem[key] === "passed") {
-          const prevCall = await t.get(initialisedDB.collection("clubs").doc(key))
+          const prevCall = await initialisedDB.collection("clubs").doc(key).get()
           const prevCount = prevCall.get("call_count") || 0
-         t.set(initialisedDB.collection("clubs").doc(key), { call_count: prevCount + 1 }, { merge: true })
+          await initialisedDB.collection("clubs").doc(key).update("call_count", prevCount + 1)
         }
         newAuditionData[key] = "rejected"
       }
