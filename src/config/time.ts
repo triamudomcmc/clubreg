1f5ab74a3403512e6137d8239f65b54512d8ee48
lukeskyflyer22 <siruj2201@gmail.com>
2025-04-06 08:52:20 +0700

    update time config 67

--- src/config/time.ts
+++ src/config/time.ts
@@ -9,25 +9,25 @@ export const schoolYear = +new Date("2025")
 // all club data update start
 export const editInitData = +new Date("2025-04-07T00:00:00.000+07:00")
 // all club data update deadline
-export const endEditInitData = +new Date("2025-04-18T00:00:00.000+07:00")
+export const endEditInitData = +new Date("2025-04-18T23:59:00.000+07:00")
 
 // initiate the dialogue before old club confirmation starts
 export const beforeOldClub = +new Date("2025-05-02T00:00:00.000+07:00")
 // starts old club confirmation
-export const startOldClub = +new Date("2025-05-03T08:00:00.000+07:00")
+export const startOldClub = +new Date("2025-05-06T08:00:00.000+07:00")
 // start counting down before old club confirmation
-export const startOldClubCountdown = +new Date("2025-05-03T00:00:00.000+07:00")
-// end old clun confirmation
-export const endOldClub = +new Date("2025-05-03T23:59:00.000+07:00")
+export const startOldClubCountdown = +new Date("2025-05-06T00:00:00.000+07:00")
+// end old club confirmation
+export const endOldClub = +new Date("2025-05-06T20:00:00.000+07:00")
 
 // export const startOldClub = +new Date("2023-05-05T11:30:00.000+07:00")
 // export const startOldClubCountdown = +new Date("2023-05-05T10:30:00.000+07:00")
 // export const endOldClub = +new Date("2023-05-07T00:00:00.000+07:00")
 
 // start register time countdown (usually, this will be updated before PAE) *optional
-export const openRegisterTime = +new Date("2024-05-07T12:00:00.000+07:00")
+export const openRegisterTime = +new Date("2025-05-16T14:00:00.000+07:00")
 // registration open time
-export const openTime = +new Date("2025-05-16T11:00:00.000+07:00")
+export const openTime = +new Date("2025-05-16T12:00:00.000+07:00")
 // end of data editing time for updating positions and audition result.
 export const editDataTime = +new Date("2025-05-25T23:59:00.000+07:00")
 // registration close 1st round.
@@ -35,7 +35,7 @@ export const endRegClubTime = +new Date("2025-05-23T23:59:00.000+07:00")
 // announce audition result. (user can select either to accept or reject)
 export const announceTime = +new Date("2025-05-26T07:30:00.000+07:00")
 // end announce time. Dicisions are no longer accepted
-export const endAnnounceTime = +new Date("2025-05-27T23:59:00.000+07:00")
+export const endAnnounceTime = +new Date("2025-05-26T23:59:00.000+07:00")
 /*
 Maintainance Break
 (usually, 00.00 - 08.00)
